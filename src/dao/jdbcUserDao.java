d491f575f2119c0130aa290e863866c80ac7cb8d
adr1ann32323 <adr1ann32323@gmail.com>
2025-10-05 19:23:36 -0500

    Feat: UserDao UserService UserController view

---
+++ src/dao/jdbcUserDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import config.DbConfig;
+import domain.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class jdbcUseDao implements userDao {
+    @Override
+    public User create(User user) throws SQLException {
+        String sql = "INSERT INTO usuario (name, email, rol) VALUES (?, ?, ?)";
+        try (Connection c = DbConfig.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getRol());
+            ps.executeUpdate();
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    user.setId(rs.getInt(1));
+                    return user;
+                }
+            }
+        }
+        throw new SQLException("No se pudo crear usuario");
+    }
+    @Override
+    public User findById(int id)  throws SQLException {
+        String sql = "SELECT id,name,email,rol FROM user WHERE id = ?";
+        try (Connection c = DbConfig.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+            ps.setInt(1,id);
+            try (ResultSet rs = ps.executeQuery()){
+                if (rs.next()) return new User(rs.getInt("id"), rs.getString("nombre"), rs.getString("email"), rs.getString("rol"));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<User> listAll()  throws SQLException {
+        ArrayList<User> listUsers = new ArrayList<>();
+        String sql = "SELECT id,name,email,rol FROM user";
+        try (Connection c = DbConfig.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                listUsers.add(new User(rs.getInt("id"), rs.getString("name"), rs.getString("email"), rs.getString("rol")));
+            }
+        }
+        return listUsers;
+    }
+}
---
+++ src/service/UserService.java
@@ -0,0 +1,26 @@
+package service;
+
+import dao.jdbcUserDao;
+import dao.userDao;
+import domain.User;
+
+import java.util.ArrayList;
+
+public class userService {
+    private final userDao userDao;
+        @Override
+        public User create(User user) throws Exception {
+            return null;
+        }
+
+        @Override
+        public ArrayList<User> listAll() throws Exception {
+            return null;
+        }
+
+        @Override
+        public User findById(int id) throws Exception {
+            return null;
+        }
+
+}
